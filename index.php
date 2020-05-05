e58b8b6c6306ef79f39414f981621f65977c6e57
Mohit1265 <64827368+Mohit1265@users.noreply.github.com>
2020-05-05 12:57:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'opt/app-root/src/classes/queryDb.php';
+require_once'http://sqlite-project-git-sqlite-project.apps.us-west-1.starter.openshift-online.com/classes/queryDb.php';
+// require_once 'opt/app-root/src/classes/queryDb.php';
 // require_once ($_ENV['OPENSHIFT_REPO_DIR'] . 'classes/queryDb.php');
 $products = getProducts(null,null,'4');
 ?>
