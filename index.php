b58e25276c6e16c929475094755e439ad400b11e
Mohit1265 <64827368+Mohit1265@users.noreply.github.com>
2020-05-05 13:50:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-// require_once'/opt/rh/php55/root/usr/share/pear/classes/queryDb.php';
-// require_once 'opt/app-root/src/classes/queryDb.php';
-echo $('SUBDOMAIN');
-exit;
 require_once 'classes/queryDb.php';
 $products = getProducts(null,null,'4');
 ?>
